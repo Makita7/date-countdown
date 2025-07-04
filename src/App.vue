f70a7c1832c6559483323b0c7f30239fceafbd01
Sofía Makita <sophiamakita@gmail.com>
2025-07-04 11:41:15 -0300

    making toggles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
+import ToggleBtn from './components/ToggleBtn.vue';
+
+let darkMode = ref(false);
+
+function toggleDarkMode() {
+  darkMode.value = !darkMode.value;
+};
+
+let monthMode = ref(false);
+
+function toggleMonthMode() {
+  monthMode.value = !monthMode.value;
+};
 
 </script>
 
@@ -14,8 +28,13 @@ import { RouterLink, RouterView } from 'vue-router'
   <Transition name="fade" mode="out-in">
     <header class="d-flex justify-between align-center">
       <p class="logo">Coundown || Date</p>
-      <nav>
-        <RouterLink to="/" class="link">Home</RouterLink>
+      <nav class="d-flex align-center">
+        <img v-if="!darkMode" src="@/assets/icon-sun-filled.png" alt="light mode on" class="icon" />
+        <img v-else src="@/assets/icon-sun.png" alt="light mode off" class="icon" />
+        <ToggleBtn @toggleEmit="toggleDarkMode" />
+        <img v-if="!darkMode"  src="@/assets/icon-moon.png" alt="dark mode off" class="icon moon" />
+        <img v-else src="@/assets/icon-moon-filled.png" alt="dark mode on" class="icon moon" />
+        <RouterLink to="/" class="link ml-2">Home</RouterLink>
       </nav>
     </header>
   </Transition>
@@ -23,9 +42,25 @@ import { RouterLink, RouterView } from 'vue-router'
   <Transition name="fade" mode="out-in">
     <RouterView />
   </Transition>
+
+  <div class="d-flex align-center justify-between mt-10">
+    <button class="d-flex align-center addNew"><span>+</span> New Date</button>
+
+    <div class="d-flex align-center monthToggle">
+      <p class="mr-2">Days</p>
+      <ToggleBtn @toggleEmit="toggleMonthMode" />
+      <p class="ml-2">Month</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+header{
+  background-color: #ffffffba;
+  padding: 0.5rem 1.5rem;
+  border-radius: 1rem;
+}
 
 .logo{
   font-size: 1.2rem;
@@ -41,6 +76,41 @@ import { RouterLink, RouterView } from 'vue-router'
 .link:hover{
   font-weight: bold;
   font-size: 1.05rem;
+}
+
+.moon{
+  max-width: 2rem;
+  max-height: 1.5rem;
+  padding: 0.5rem;
+}
+
+.monthToggle{
+  background-color: #ffffffba;
+  padding: 0.5rem 1.5rem;
+  border-radius: 1rem;
+}
+
+.addNew{
+  background-color: #ffffffba;
+  border: none;
+  padding: 0rem 1rem;
+  font-size: 1rem;
+  border-radius: 1rem;
+  height: 3rem;
+}
+
+button.d-flex.align-center.addNew:hover {
+    background-color: #feedcb;
+    font-weight: bold;
+}
+
+button.d-flex.align-center.addNew:active {
+    background-color: #edcf92;
+}
+
+.addNew span{
+  margin-right: 0.5rem;
+  font-size: 1.5rem;
 }
 
 </style>
---
+++ src/components/ToggleBtn.vue
@@ -0,0 +1,81 @@
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+defineEmits(['toggleEmit'])
+
+
+</script>
+
+<template>
+    <label class="switch">
+        <input @click="$emit('toggleEmit')" type="checkbox">
+        <span class="slider round"></span>
+    </label>
+</template>
+
+<style>
+/* The switch - the box around the slider */
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 53px;
+    height: 28px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+/* The slider */
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #aa796d;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 20px;
+    width: 19px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+input:checked + .slider {
+    background-color: #17203e;
+}
+
+input:focus + .slider {
+    box-shadow: 0 0 1px #17203e;
+}
+
+input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
+
+
+/* Rounded sliders */
+.slider.round {
+    border-radius: 34px;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+}
+</style>
